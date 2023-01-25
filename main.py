4f315a6fca449e95bd59231c71d42b558fb7c1f8
YaruTK <mcdonalddark@gmail.com>
2023-01-25 16:07:32 +0100

    placement of ships added

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
-# This is a sample Python script.
+# class for player
+# class for field
+# method to print field out
+# class for ships
+import parameters
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+list_of_sheep = []
 
 
-# Press the green button in the gutter to run the script.
+class Field:
+    def __init__(self):
+        self.width = parameters.width
+        self.height = parameters.height
+        self.form = parameters.form
+        self.matrix = create_empty_map()
+
+    def show(self):
+        for row in range(self.height):
+            for col in range(self.width):
+                print(str(self.matrix[row][col]) + ' ', end="")
+            print('\n')
+
+
+class Sheep:
+    def __init__(self, head, tail):
+        x1, y1 = head[0]-1, head[1]-1
+        x2, y2 = tail[0]-1, tail[1]-1
+        if y1 == y2:
+            self.horizontal = True
+            self.length = abs(x2 - x1) + 1
+        else:
+            self.horizontal = False
+            self.length = abs(y2 - y1) + 1
+
+        self.temporal_shipmap = create_empty_map()
+        if self.horizontal:
+            for x in range(min(x1, x2), min(x1, x2) + self.length):
+                self.temporal_shipmap[x][y1] = 2
+        else:
+            for y in range(min(y1, y2), min(y1, y2) + self.length):
+                self.temporal_shipmap[x1][y] = 2
+        list_of_sheep.append(self)
+
+    def __del__(self):
+        try:
+            list_of_sheep.remove(self)
+        except: # probably it should be written differently
+            print("There was a problem deleting file")
+
+    def place(self, field_class):
+        for row in range(parameters.height):
+            for col in range(parameters.width):
+                field_class.matrix[row][col] = field_class.matrix[row][col] + self.temporal_shipmap[row][col]
+        if max(map(max, field_class.matrix)) > 2:
+            print("There are some overlaps in sheep")
+
+
+def create_empty_map():
+    temp_map = [[0 for col in range(parameters.width)] for row in range(parameters.height)]
+    return temp_map
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    field = Field()
+    ship1 = Sheep((2, 2), (2, 3))
+    ship2 = Sheep((6, 6), (6, 9))
+    for ship in list_of_sheep:
+        ship.place(field)
+    field.show()
---
+++ parameters.py
@@ -0,0 +1,3 @@
+width = 10
+height = 10
+form = "text"  # text or digital
