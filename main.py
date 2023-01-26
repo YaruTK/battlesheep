35413d1966b2a5b6c2db5595fcf2ad1a4dff14fa
YaruTK <mcdonalddark@gmail.com>
2023-01-26 16:20:52 +0100

    now we can shoot sheep, cleaned some trash code

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ dictionary_of_ships = {1: [], 2: [], 3: [], 4: []}
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 letters_lowercase = "abcdefghijklmnopqrstuvwxyz"
 separators = " .-=+/?!@#$%^&*()_\\"
-roman = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX','X']
+roman = ['    ', '   I', '  II', ' III', '  IV', '   V', '  VI', ' VII', 'VIII', '  IX', '   X']
 graphix = {-1: "o",
            0: ".",
            1: "*",
@@ -69,7 +69,7 @@ class Shot:
 
 
 class Sheep:
-    def __init__(self, head, tail):
+    def __init__(self, head, tail, f: Field):
         self.topleft = (min(head[0]-1, tail[0]-1), min(head[1]-1, tail[1]-1))
         self.bottomright = (max(head[0]-1, tail[0]-1), max(head[1]-1, tail[1]-1))
         self.spawnbox = (
@@ -84,22 +84,18 @@ class Sheep:
             self.length = abs(self.topleft[0] - self.bottomright[0]) + 1
             self.tiles = [(x, self.topleft[1]) for x in range(self.topleft[0], self.topleft[0] + self.length)]
         self.hp = 2 * self.length
-        self.temporal_shipmap = create_empty_map()
 
         if check_existence_possibility(self) and check_intersection(self) and check_size(self):
             for x in range(self.spawnbox[0][0], self.spawnbox[1][0] + 1):
                 for y in range(self.spawnbox[0][1], self.spawnbox[1][1] + 1):
                     intersections[x][y] = 1
-            for item in self.tiles:
-                x, y = item
-                self.temporal_shipmap[x][y] = 2
-            add_ship(self)
+            add_ship(self, f)  # also triggers self.place(f)
             self.alive = True
 
     def place(self, field_class):
-        for row in range(config.height):
-            for col in range(config.width):
-                field_class.matrix[row][col] += self.temporal_shipmap[row][col]
+        for item in self.tiles:
+            x, y = item
+            field_class.matrix[x][y] += 2
 
     def check_hp(self, f: Field):
         if self.hp <= self.length:
@@ -111,7 +107,6 @@ class Sheep:
         for x in range(self.spawnbox[0][0], self.spawnbox[1][0] + 1):
             for y in range(self.spawnbox[0][1], self.spawnbox[1][1] + 1):
                 temp_shot = Shot(x, y, f)  # just let machine shoot for you!
-
 
 
 def check_existence_possibility(newship: Sheep):
@@ -139,9 +134,9 @@ def check_size(newship: Sheep):
         return False
 
 
-def add_ship(ship):
-    dictionary_of_ships[ship.length].append(ship)
-    ship.place(field)
+def add_ship(s: Sheep, f: Field):
+    dictionary_of_ships[s.length].append(s)
+    s.place(f)
 
 
 def translate(input):
@@ -161,7 +156,7 @@ def cut_coordinates(str):
     return 0, 0
 
 
-def announce_ships():
+def announce_ships():  # there will be second announcer how many are still alive, might need separate class
     message = "Singular sheep: " + str(len(dictionary_of_ships[1])) + '/' + str(config.existence_of_the_ships[1]) + \
         '\n' + "Double sheep: " + str(len(dictionary_of_ships[2])) + '/' + str(config.existence_of_the_ships[2]) +\
         '\n' + "Triple sheep: " + str(len(dictionary_of_ships[3])) + '/' + str(config.existence_of_the_ships[3]) +\
@@ -173,14 +168,16 @@ if __name__ == '__main__':
     field = Field()
 
     field.show()
+    # adding all 10 ships manually
     while count_ships(dictionary_of_ships) < sum(config.existence_of_the_ships.values()):
         print(announce_ships())
         temp_ship = input("Choose coordinates of the battlesheep (format \'A6 C6\'): ")  # variety of separators avail.
         head_txt, tail_txt = cut_coordinates(temp_ship)
         headx, tailx = translate(head_txt), translate(tail_txt)  # translate is used in shooting too,
         # that is the reason why it is not part of cutting coordinates
-        shipx = Sheep(headx, tailx)  # adding new ship to dictionary
-        field.show()
+        shipx = Sheep(headx, tailx, field)  # adding new ship to dictionary
+        field.show()  # refreshing field
+    # kinda shooting, wip
     for x in range(10):  # conditions to be added later
         temp_shot = input("Choose where to shoot (format \'A6\'): ")
         tempx, tempy = translate(temp_shot)  # we get x and y coordinates of a shot
